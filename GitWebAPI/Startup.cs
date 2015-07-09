18659ec11e68de5dc74fb56ef102554d8a697f86
jaan@itbase.ee <jaan@itbase.ee>
2015-07-09 22:12:46 +0200

    1.0

---
+++ GitWebAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitWebAPI.Startup))]
+namespace GitWebAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
